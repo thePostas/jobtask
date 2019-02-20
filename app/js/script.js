4067e8235dbd2a8f8cd1ced9ebec986cf4f78663
Postas <postas87@mail.ru>
2019-02-20 20:49:31 +0300

    making card block

--- app/js/script.js
+++ app/js/script.js
@@ -3,9 +3,11 @@ navigationButtons = document.getElementsByClassName('menu__list-link');
 for (var i = 0; i < navigationButtons.length; i++) ((i)=>{
     navigationButtons[i].addEventListener('click', function () {
         clear();
-        navigationButtons[i].classList.add('menu__list-link_active');
-        navigationButtons[i].style.backgroundColor = '#2a2d37';
-        if(i > 0) {
+        if (i < navigationButtons.length-1) {
+            navigationButtons[i].classList.add('menu__list-link_active');
+            navigationButtons[i].style.backgroundColor = '#2a2d37';
+        }
+        if(i > 0 && i < navigationButtons.length-1) {
             navigationButtons[i - 1].style.backgroundColor = "#1f2229";
         }
         if (i < navigationButtons.length-1) {
@@ -16,7 +18,7 @@ for (var i = 0; i < navigationButtons.length; i++) ((i)=>{
 
 function clear() {
     for (var i = 0; i < navigationButtons.length; i++) {
-        navigationButtons[i].style.backgroundColor = '#191c24';
         navigationButtons[i].classList.remove('menu__list-link_active');
+        navigationButtons[i].style.backgroundColor = '#191c24';
     }
 }
