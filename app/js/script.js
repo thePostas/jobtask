1884c825004bb3222954c44b0bf620422c6e6c5c
Postas <postas87@mail.ru>
2019-02-20 16:45:21 +0300

    navigation block completed

---
+++ app/js/script.js
@@ -0,0 +1,22 @@
+navigationButtons = document.getElementsByClassName('menu__list-link');
+
+for (var i = 0; i < navigationButtons.length; i++) ((i)=>{
+    navigationButtons[i].addEventListener('click', function () {
+        clear();
+        navigationButtons[i].classList.add('menu__list-link_active');
+        navigationButtons[i].style.backgroundColor = '#2a2d37';
+        if(i > 0) {
+            navigationButtons[i - 1].style.backgroundColor = "#1f2229";
+        }
+        if (i < navigationButtons.length-1) {
+            navigationButtons[i + 1].style.backgroundColor = "#1f2229";
+        }
+    })
+})(i);
+
+function clear() {
+    for (var i = 0; i < navigationButtons.length; i++) {
+        navigationButtons[i].style.backgroundColor = '#191c24';
+        navigationButtons[i].classList.remove('menu__list-link_active');
+    }
+}
